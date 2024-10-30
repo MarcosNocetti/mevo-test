0cbbc0865e401758d7759aac8933acb71fb071d0
Marcos Nocetti <work@marcosnocetti.com>
2024-10-29 21:31:36 -0300

    Update migration.sql

--- src/migrations/migration.sql
+++ src/migrations/migration.sql
@@ -9,6 +9,11 @@ CREATE TABLE IF NOT EXISTS operations (
     reason VARCHAR(255)
 );
 
+CREATE TABLE IF NOT EXISTS users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL UNIQUE
+);
 
 DO $$ 
 BEGIN 
@@ -20,5 +25,18 @@ BEGIN
     ) THEN
         ALTER TABLE operations 
         ADD CONSTRAINT unique_operation UNIQUE (sender, receiver, amount, suspicious, reason);
+    END IF;
+END $$;
+
+DO $$ 
+BEGIN 
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM pg_constraint 
+        WHERE conname = 'unique_user' 
+        AND conrelid = 'users'::regclass
+    ) THEN
+        ALTER TABLE users 
+        ADD CONSTRAINT unique_user UNIQUE (name);
     END IF;
 END $$;

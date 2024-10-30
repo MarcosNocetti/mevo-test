f9b10c2de509cf0e39c8eb8008d727ec210351a5
Marcos Nocetti <work@marcosnocetti.com>
2024-10-29 21:24:52 -0300

    add controller auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, Body, Request, UseGuards, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+
+  async login(@Body() req: any) {
+    const user = await this.authService.validateUser(req.name, req.password);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return this.authService.login(user);
+  }
+}
